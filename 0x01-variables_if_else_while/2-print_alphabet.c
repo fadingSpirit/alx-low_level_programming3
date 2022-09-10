bd542bd98ffeb0b25d30e782665fdc22a5395eb1
fadingSpirit <nonyerem.nuel@gmail.com>
2022-09-10 16:53:41 -0700

    Variable

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,37 @@
+nclude <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return:0
+ *
+ *     */
+
+
+
+int main(void)
+
+{
+
+		int alpha;
+
+
+
+			for (alpha = 'a'; alpha <= 'z'; alpha++)
+
+					{
+
+								putchar(alpha);
+
+									}
+
+				putchar('\n');
+
+					return (0);
+
+}
---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Entry
+ *
+ *   *
+ *
+ *    * Return: 0
+ *
+ *     */
+
+
+
+int main(void)
+
+{
+
+		int alpha;
+
+
+
+			for (alpha = 'a'; alpha <= 'z'; alpha++)
+
+					{
+
+								if (alpha != 'e' && alpha != 'q')
+
+											{
+
+															putchar(alpha);
+
+																	}
+
+										else
+
+													{
+
+																}
+
+											}
+
+				putchar('\n');
+
+					return (0);
+
+}
