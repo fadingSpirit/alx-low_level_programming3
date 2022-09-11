6ab4b39ecddabf3dcbc797cf87fac135d09e94bc
fadingSpirit <110996685+fadingSpirit@users.noreply.github.com>
2022-09-10 17:02:13 -0700

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,59 +1,30 @@
 #include <stdio.h>
 
-
-
 /**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: 0
- *
- *     */
-
-
+ * Return: 0
+ */
 
 int main(void)
-
 {
+	int a, i;
 
-		int a, i;
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (i = '0'; i <= '9'; i++)
+			if (a < i)
+			{
+				putchar(a);
+				putchar(i);
 
-
-
-			for (a = '0'; a <= '9'; a++)
-
-					{
-
-								for (i = '0'; i <= '9'; i++)
-
-												if (a < i)
-
-																{
-
-																					putchar(a);
-
-																									putchar(i);
-
-
-
-																													if (a != '8' || (a == '8' && i != '9'))
-
-																																		{
-
-																																								putchar(',');
-
-																																													putchar(' ');
-
-																																																	}
-
-																																}
-
-										}
-
-				putchar('\n');
-
-					return (0);
-
+				if (a != '8' || (a == '8' && i != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	putchar('\n');
+	return (0);
 }
