ba757f7a34aff712b2c4b779c17f9d976e775ee9
fadingSpirit <nonyerem.nuel@gmail.com>
2022-09-13 16:17:05 -0700

    print alphabet in lowercase

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - to print alphabet
+ *
+ * Return: void
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+        char c;
+        int num = 0;
+
+        while (num <= 9)
+        {
+		for (c = 'a'; c <= 'z'; c++)
+                {
+			_putchar(c);
+		}                
+		num++;
+                _putchar('\n');
+        }
+}
