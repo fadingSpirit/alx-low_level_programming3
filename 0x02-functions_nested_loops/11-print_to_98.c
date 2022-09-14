bd92323f17dc9617a65335dddc9285aee3f3730f
fadingSpirit <110996685+fadingSpirit@users.noreply.github.com>
2022-09-13 20:59:46 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,20 +10,16 @@
 void print_to_98(int n)
 {
 	if (n < 98)
-
 	{
 		while (n < 98)
-
 		{
 			printf("%d, ", n);
 			n++;
 		}
 	}
 	else if (n > 98)
-
 	{
 		while (n > 98)
-	
 		{
 			printf("%d, ", n);
 			n--;
