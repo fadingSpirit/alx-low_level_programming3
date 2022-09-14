cb327348567c6bbf60d3324721f85ad52502040b
fadingSpirit <110996685+fadingSpirit@users.noreply.github.com>
2022-09-13 19:58:43 -0700

    updating 24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,19 +15,11 @@ void jack_bauer(void)
 	while (hours_tens < '3')
 	{
 		if (hours_tens == '2')
-<<<<<<< HEAD
-
-=======
->>>>>>> c2eaa5d711c27b834ddbddb741c4ed72dd74815c
 		{
 			hours_max = '4';
 		}
 		hours_ones = '0';
 		while (hours_ones < hours_max)
-<<<<<<< HEAD
-
-=======
->>>>>>> c2eaa5d711c27b834ddbddb741c4ed72dd74815c
 		{
 			minutes_tens = '0';
 			while (minutes_tens < '6')
