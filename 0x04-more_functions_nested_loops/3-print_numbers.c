ef229412a1e0dc3fd01fd364469bb38efcbf5171
fadingSpirit <110996685+fadingSpirit@users.noreply.github.com>
2022-09-15 07:09:57 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints number 0 to 9
+ * return: 0
+ */
+
+void print_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
