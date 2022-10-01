0971eac8a6142330dcd3f4532c6411e9f5521cac
fadingSpirit <nonyerem.nuel@gmail.com>
2022-09-30 21:20:36 -0700

    All of Argc and Argv files.

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - multiplies two numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int a = atoi(argv[1]);
+	int b = atoi(argv[2]);
+	int result;
+
+	if (argc == 3)
+	{
+		result = a * b;
+		printf("%d\n", result);
+		return (0);
+	}
+	printf("Error\n");
+	return (1);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This readme constains the argc and argv files
